86762007db734227e89f227a151dc8c5f1e372b6
tchrystak <chrystaktrey@gmail.com>
2022-09-16 15:33:03 -0700

    Enhanced Card implementation, started Deck.

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -1,6 +1,8 @@
 package com.tlglearning.cards.model;
 
-public class Card {
+import java.util.Objects;
+
+public class Card implements Comparable<Card> {
 
   private final Rank rank; // field declaration: modifiers, field , type
   private final Suit suit;
@@ -21,10 +23,32 @@ public class Card {
   }
 
   @Override
+  public int hashCode() {
+    return Objects.hash(rank, suit); // FIXME: 9/16/2022 Move this to initialization.
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean result;
+    if (this == obj) {
+      result = true;
+    } else if (obj instanceof Card) {
+      Card other = (Card) obj;
+      result = (this.rank == other.rank && this.suit == other.suit);
+    } else {
+      result = false;
+    }
+    return result;
+  }
+
+  @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
   }
 
-  // TODO: 9/13/2022 Override equals method. 
-  // TODO: 9/13/2022 Override hashCode method. 
+  @Override
+  public int compareTo(Card other) {
+    return 0; // O means equal
+    // TODO: 9/16/2022  Replace with comparison based on suit and rank.
+  }
 }
